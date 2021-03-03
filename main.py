f68b39da5be01bdc283e520d996c2826cc636e65
ClevelandLung <79961082+ClevelandLung@users.noreply.github.com>
2021-03-03 20:02:12 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from myMath import myStatistics
+number1 = int(input('輸入第一個數: '))
+number2 = int(input('輸入第二個數: '))
+number3 = int(input('輸入第三個數: '))
+number4 = int(input('輸入第四個數: '))
+number5 = int(input('輸入第五個數: '))
+s = (number1,number2,number3,number4,number5)
+print(myStatistics.myMean(s))
